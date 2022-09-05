9fc35c06aba76c6752eb6c7375ec518c31a73d2c
dev-farrahv <dev.farrahv@gmail.com>
2022-09-06 03:20:58 +0800

    change return status

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+const port = 3000;                  //Save the port number where your server will be listening
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/studentInfo', (req, res) => {        //get requests to the root ("/") will route here
-    return res.status(200).send();      //server responds by sending the index.html file to the client's browser
+    console.log("sample log return");
+    return res.status(200).send("Success return");      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+    
 });
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
